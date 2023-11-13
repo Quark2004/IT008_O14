ea24bec0faba9145da2cc2b21e01b3caae6a63dd
YaDat354 <22520243@gm.uit.edu.vn>
2023-11-13 22:45:52 +0700

    chua biet cach de ket noi voi postgreSQL nen chua hoan thien duoc

---
+++ Documents/Quantrivienv.1/fLogin.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Quantrivienv._1
+{
+    public partial class fLogin : Form
+    {
+        public fLogin()
+        {
+            InitializeComponent();
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit(); 
+        }
+
+        private void fLogin_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void fLogin_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if(MessageBox.Show("Bạn có thật sự muốn thoát chương trình?","Thông báo", MessageBoxButtons.OKCancel) != System.Windows.Forms.DialogResult.OK)
+            {
+                e.Cancel = true;
+            }
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            string userName = txbUsername.Text;
+            string password = txbPassword.Text;
+            if(userName == "")
+            {
+                MessageBox.Show("Yêu cầu nhập tên đăng nhập!");
+                return;
+            }
+            if(password == "")
+            {
+                MessageBox.Show("Yêu cầu nhập mật khẩu!");
+                return;
+            }
+            fAdmin f= new fAdmin();
+            this.Hide();
+            f.ShowDialog();
+            this.Close();
+        }
+    }
+}
